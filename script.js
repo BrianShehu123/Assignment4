d78cae5de5e1553a67d3549414d388ec29686924
Brian Shehu <brian.shehu2@gmail.com>
2023-06-21 23:03:51 -0400

    finished

--- script.js
+++ script.js
@@ -1,53 +1,83 @@
 // Retrieve DOM elements
 // TODO: Get references to the required DOM elements using document.getElementById()
+const poster = document.querySelector('.poster');
+const img = poster.querySelector('#poster-image');
+const title = poster.querySelector('#poster-title');
+const quote = poster.querySelector('#poster-quote');
+
+const posterForm = document.querySelector("#poster-form");
 
 // Array of predefined poster objects
 const posters = [
-    {
-      image: 'image-url-1.jpg',
-      title: 'Poster 1',
-      quote: 'Quote 1',
-    },
-    {
-      image: 'image-url-2.jpg',
-      title: 'Poster 2',
-      quote: 'Quote 2',
-    },
-    {
-      image: 'image-url-3.jpg',
-      title: 'Poster 3',
-      quote: 'Quote 3',
-    },
-    // Add more poster objects as needed
-  ];
-  
-  // Event listener for randomize button
-  // TODO: Add an event listener to the randomizeButton that calls a function when clicked
-  
-  // Event listener for submit button
-  // TODO: Add an event listener to the submitButton that calls a function when clicked
-  
-  // Function to generate a random poster
-  function generateRandomPoster() {
+  {
+    image: 'https://media.istockphoto.com/id/1255203350/photo/teamwork-friendship-hiking-help-each-other-trust-assistance-silhouette-in-mountains-sunrise.jpg?s=612x612&w=0&k=20&c=XU9MhkkMdyM59gAMDSUwltr-LhVyjWHLvTxQCPwVbF8=',
+    title: 'Climbing the top',
+    quote: 'The hardest mountain to climb is the one within',
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1548438294-1ad5d5f4f063?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1744&q=80',
+    title: 'Dream Big',
+    quote: 'Anyone can dream big',
+  },
+  {
+    image: 'https://i.etsystatic.com/34105328/r/il/380455/4018386002/il_fullxfull.4018386002_29nj.jpg',
+    title: 'Lightbulb',
+    quote: 'Good idea for big opportunity',
+  },
+  // Add more poster objects as needed
+];
+
+// Event listener for randomize button
+// TODO: Add an event listener to the randomizeButton that calls a function when clicked
+let pushBtn = document.querySelector("#randomize");
+pushBtn.addEventListener("click", (event) => {
+    generateRandomPoster();
+});
+
+// Event listener for submit button
+// TODO: Add an event listener to the submitButton that calls a function when clicked
+let submitBtn = posterForm.querySelector("#submit");
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    generateCustomPoster(event);
+    posterForm.reset();
+});
+
+// Function to generate a random poster
+function generateRandomPoster() {
+    
     // TODO: Generate a random index within the range of the posters array length
-  
+    let ranIndex = Math.floor(Math.random() * posters.length);
+
     // TODO: Retrieve the random poster object from the posters array
-  
+    let ranPoster = posters[ranIndex];
+
     // TODO: Call the function to update the DOM with the values from the random poster object
-  }
-  
-  // Function to generate a custom poster
-  function generateCustomPoster(event) {
+    updatePoster(ranPoster.image, ranPoster.title, ranPoster.quote);
+}
+
+// Function to generate a custom poster
+function generateCustomPoster(event) {
     event.preventDefault();
   
     // TODO: Retrieve the entered quote and author from the input fields
-  
+    let formTitle = document.getElementById("poster-form-title").value
+    let formQuote = document.getElementById("poster-form-quote").value
+    let imgLink = document.getElementById("poster-form-imgLink").value
+
     // TODO: Create a custom poster object with the entered values
-  
+    posters.push({image: imgLink, title: formTitle, quote: formQuote});
+
     // TODO: Call the function to update the DOM with the values from the custom poster object
-  }
-  
-  // Function to update the poster content in the DOM
-  function updatePoster(imageUrl, title, quote) {
+    updatePoster(imgLink, formTitle, formQuote);
+
+
+}
+
+// Function to update the poster content in the DOM
+function updatePoster(imageUrl, title, quote) {
     // TODO: Update the DOM with the values provided for the poster image, title, and quote
-  }
+    img.setAttribute("src", imageUrl);
+    title.textContent = title;
+    quote.textContent = quote;
+}
